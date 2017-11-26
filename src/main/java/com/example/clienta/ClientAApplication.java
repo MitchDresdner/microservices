9d06a306613291b223ae180a856a74b36ecad058
Mitch Dresdner <mitchd@mindspring.com>
2017-11-26 16:43:51 -0500

    Spring boot demo client for Eureka, initial checkin

---
+++ src/main/java/com/example/clienta/ClientAApplication.java
@@ -0,0 +1,42 @@
+package com.example.clienta;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@EnableDiscoveryClient
+@RestController
+@SpringBootApplication
+public class ClientAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientAApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String name() {
+		return "Client-A";
+	}
+}
+
+//@RestController
+//class ServiceInstanceRestController {
+//
+//	@Autowired
+//	private DiscoveryClient discoveryClient;
+//
+//	@RequestMapping("/service-instances/{applicationName}")
+//	public List<ServiceInstance> serviceInstancesByApplicationName(
+//			@PathVariable String applicationName) {
+//		return this.discoveryClient.getInstances(applicationName);
+//	}
+//}
